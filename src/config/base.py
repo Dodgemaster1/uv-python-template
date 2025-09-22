48d6d6987be130939e420470b3ff4d5ae646cd35
Dodgemaster1 <mykyta.manaiev@gmail.com>
2025-09-22 16:57:21 +0300

    update base.py: add case sensitivity to BaseApplicationConfig

--- src/config/base.py
+++ src/config/base.py
@@ -8,7 +8,7 @@ from src.utils.dirs import BASE_DIR
 class BaseApplicationConfig(BaseSettings):
     model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
-        case_sensitive=False,
+        case_sensitive=True,
         extra="ignore",
         env_file_encoding="utf-8",
     )
